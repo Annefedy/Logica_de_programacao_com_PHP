8ebcfce8c20b13b5a868e6a45ec2b2fcc6b79c9b
annefedy <annecarine1997@gmail.com>
2023-02-08 11:03:13 -0300

    Question 04

--- Question04.php
+++ Question04.php
@@ -1,2 +1,12 @@
 <!-- 04 - Solicite a entrada de um número e descubra se um número digitado é par
  ou ímpar. -->
+
+<?php
+
+$numero = readline('Digite um número: ');
+
+if ($numero % 2 == 0) {
+  echo "O número é par";
+} else {
+  echo "O número é ímpar";
+}
