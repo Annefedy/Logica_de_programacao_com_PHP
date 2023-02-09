7dadcace4fbda33426b1a40bcfb76a7de0f18085
annefedy <annecarine1997@gmail.com>
2023-02-09 13:59:03 -0300

    Question 13

--- Question13.php
+++ Question13.php
@@ -1,2 +1,19 @@
 <!-- 13 - Faça um script que peça a temperatura em graus Farenheit, 
 transforme e mostre a temperatura em graus Celsius. -->
+
+<?php
+
+// Declaração da variável para armazenar a temperatura em Farenheit
+$tempFarenheit = 0;
+
+// Pede a temperatura em Farenheit e armazena na variável
+echo "Digite a temperatura em graus Farenheit: ";
+$tempFarenheit = readline();
+
+// Calcula a temperatura em Celsius
+$tempCelsius = ($tempFarenheit - 32) * 5 / 9;
+
+// Exibe o resultado
+echo "A temperatura em Celsius é: $tempCelsius";
+
+?>
