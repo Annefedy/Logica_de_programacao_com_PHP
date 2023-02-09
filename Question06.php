d00f1cc69d777aa8b9c59f8555cd74a634287f79
annefedy <annecarine1997@gmail.com>
2023-02-09 10:58:33 -0300

    Question 06

--- Question06.php
+++ Question06.php
@@ -1,3 +1,14 @@
 <!-- 06 - Faça um algoritmo em PHP onde verifica se o valor da variável A é maior
  ou menor que o valor da variável B. A mensagem a ser impressa deve ser 
  "A maior que B" ou "A menor que B". -->
+
+<?php
+$A = 10;
+$B = 20;
+
+if ($A > $B) {
+  echo "A é maior que B";
+} else {
+  echo "A é menor que B";
+}
+?>
