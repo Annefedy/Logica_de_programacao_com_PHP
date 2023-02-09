a27bdee9514ddf3c7f96c2f70aed46e9505cf463
annefedy <annecarine1997@gmail.com>
2023-02-09 10:39:36 -0300

    Question 05

--- Question05.php
+++ Question05.php
@@ -1,3 +1,14 @@
 <!-- 05 - Faça um algoritmo PHP que receba os valores A e B, 
 imprima-os em ordem crescente em relação aos seus valores. 
 Exemplo, para A=5, B=4. Você deve imprimir na tela: "4 5". -->
+
+<?php
+$A = 5;
+$B = 4;
+
+if ($A > $B) {
+  echo $B . ' ' . $A;
+} else {
+  echo $A . ' ' . $B;
+}
+?>
