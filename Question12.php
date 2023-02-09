eeb6a502ad2f8828c2b46fd6338bced1ce254a7b
annefedy <annecarine1997@gmail.com>
2023-02-09 13:33:49 -0300

    Question 12

--- Question12.php
+++ Question12.php
@@ -1,3 +1,18 @@
 <!-- 12 - Faça um script que pergunte quanto você ganha por hora 
 e o número de horas trabalhadas no mês. 
 Calcule e mostre o total do seu salário no referido mês. -->
+
+
+<?php
+
+echo "Quantos você ganha por hora? \n";
+$ganhoPorHora = readline();
+
+echo "Quantas horas você trabalhou no mês? \n";
+$horasTrabalhadas = readline();
+
+$salarioMensal = $ganhoPorHora * $horasTrabalhadas;
+
+echo "Seu salário no mês foi de R$ " . $salarioMensal;
+
+?>
