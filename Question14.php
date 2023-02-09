188d6574dc52f307d3e16efd9633e34b6fb11c0a
annefedy <annecarine1997@gmail.com>
2023-02-09 14:09:30 -0300

    Question 14

--- Question14.php
+++ Question14.php
@@ -2,3 +2,16 @@
 construa um script que calcule seu peso ideal, utilizando as seguintes fórmulas:
     Fórumla para homens: (72.7*h) - 58
     Fórumla para mulheres: (62.1*h) - 44.7 -->
+
+<?php
+echo "Qual é sua altura: ";
+
+$h = readline();
+
+$pesoIdealHomens = (72.7 * $h) - 58;
+echo "Peso Ideal para homens: $pesoIdealHomens kg \n";
+
+$pesoIdealMulheres = (62.1 * $h) - 44.7;
+echo "Peso Ideal para mulheres: $pesoIdealMulheres kg \n";
+
+?>
