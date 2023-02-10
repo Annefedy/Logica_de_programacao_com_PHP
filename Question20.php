7df65663d702aeaf453dac74d9ccb162008f2f86
annefedy <annecarine1997@gmail.com>
2023-02-10 08:30:14 -0300

    Question 20

--- Question20.php
+++ Question20.php
@@ -2,4 +2,17 @@
 o salário reajustado de um funcionário de acordo com a seguinte regra:
 • salários até 300, reajuste de 50%
 • salários maiores que 300, reajuste de 30%
-UTILIZAR LAÇOS DE REPETIÇÃO – for() – while() e Funções de String -->
+-->
+
+<?php
+echo "Qual é seu salario? ";
+$salario = readline();
+
+if ($salario <= 300) {
+  $salario_reajustado = $salario + ($salario * 0.5);
+  echo "O salário reajustado é: " . $salario_reajustado;
+} else {
+  $salario_reajustado = $salario + ($salario * 0.3);
+  echo "O salário reajustado é: " . $salario_reajustado;
+}
+?>
