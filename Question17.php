a72307d19822a9c2cf96a4312478108ae45a7666
annefedy <annecarine1997@gmail.com>
2023-02-10 07:50:40 -0300

    Question 17

--- Question17.php
+++ Question17.php
@@ -11,3 +11,48 @@ comprar apenas galões de 3,6 litros;
 misturar latas e galões, de forma que o preço seja o menor. 
 Acrescente 10% de folga e sempre arredonde os valores para cima, 
 isto é, considere latas cheias. -->
+
+<?php
+
+$area = readline("Qual é o tamanho em metros quadrados da área, você quer pintar? ");
+
+// Cálculo do litros de tinta necessários
+$litros = $area / 6;
+
+// Cálculo de latas de 18 litros
+$latas = ceil($litros / 18);
+
+// Cálculo de galões de 3,6 litros
+$galoes = ceil($litros / 3.6);
+
+// Cálculo de mistura de latas e galões
+$multiplicadorLata = ceil($litros / 18);
+$resto = $litros % 18;
+$multiplicadorGalao = ceil($resto / 3.6);
+
+// Preços
+$precoLata = $latas * 80;
+$precoGalao = $galoes * 25;
+$precoMistura = ($multiplicadorLata * 80) + ($multiplicadorGalao * 25);
+
+// Adiciona 10% de folga
+$litrosFolga = $litros * 1.1;
+$latasFolga = ceil($litrosFolga / 18);
+$galoesFolga = ceil($litrosFolga / 3.6);
+$multiplicadorLataFolga = ceil($litrosFolga / 18);
+$restoFolga = $litrosFolga % 18;
+$multiplicadorGalaoFolga = ceil($restoFolga / 3.6);
+$precoLataFolga = $latasFolga * 80;
+$precoGalaoFolga = $galoesFolga * 25;
+$precoMisturaFolga = ($multiplicadorLataFolga * 80) + ($multiplicadorGalaoFolga * 25);
+
+echo "Para pintar uma área de $area metros quadrados, serão necessários $litros litros de tinta. \n";
+echo "Opção 1: Comprar apenas latas de 18 litros ($latas latas). Preço total: R$$precoLata.\n";
+echo "Opção 2: Comprar apenas galões de 3,6 litros ($galoes galões). Preço total: R$$precoGalao.\n";
+echo "Opção 3: Misturar latas e galões ($multiplicadorLata latas e $multiplicadorGalao galões). Preço total: R$$precoMistura.\n";
+echo "Com 10% de folga:\n";
+echo "Opção 1: Comprar apenas latas de 18 litros ($latasFolga latas). Preço total: R$$precoLataFolga.\n";
+echo "Opção 2: Comprar apenas galões de 3,6 litros ($galoesFolga galões). Preço total: R$$precoGalaoFolga.\n";
+echo "Opção 3: Misturar latas e galões ($multiplicadorLataFolga latas e $multiplicadorGalaoFolga galões). Preço total: R$$precoMisturaFolga.\n";
+
+?>
