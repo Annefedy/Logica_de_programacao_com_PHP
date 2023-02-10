e37bef7f88d3445af9c8b82796ad744c9e66e355
annefedy <annecarine1997@gmail.com>
2023-02-10 08:52:29 -0300

    Question 21

--- Question21.php
+++ Question21.php
@@ -2,3 +2,14 @@
 Efetue um algoritmo em PHP que receba um valor qualquer 
 e imprima os valores de 0 até o valor recebido, 
 exemplo: • Valor recebido = 9 • Impressão do programa – 0 1 2 3 4 5 6 7 8 9 -->
+
+
+<?php
+echo "Insirir qualquer valor: ";
+$valor = readline();
+
+for ($i = 0; $i <= $valor; $i++) {
+  echo $i . ' ';
+}
+
+?>
