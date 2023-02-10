134f9769321a70d508199e6a8b207153b4df967b
annefedy <annecarine1997@gmail.com>
2023-02-10 09:13:59 -0300

    Question 23

--- Question23.php
+++ Question23.php
@@ -2,3 +2,21 @@
 Resolva um Exercício em PHP que receba dois valores quaisquer 
 e efetue sua multiplicação utilizando para isso apenas o operador "+", 
 visto que: (3 * 5) = 5 + 5 + 5 (4 * 12) = 12 + 12 + 12 + 12 -->
+
+<?php
+
+echo "Digite o primeiro valor: ";
+$num1 = readline();
+
+echo "Dgite o segundo valor: ";
+$num2 = readline();
+
+$resultado = 0;
+
+for ($i = 0; $i < $num2; $i++) {
+  $resultado = $resultado + $num1;
+}
+
+echo "A multiplicação de $num1 x $num2 é igual a $resultado";
+
+?>
