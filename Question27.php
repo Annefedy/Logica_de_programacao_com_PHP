dfb5242abb4f2db28059a5bebed2b5c3d40887e7
annefedy <annecarine1997@gmail.com>
2023-02-10 09:42:31 -0300

    Question 27

--- Question27.php
+++ Question27.php
@@ -1,4 +1,26 @@
 <!-- 27 - UTILIZAR LAÇOS DE REPETIÇÃO – for() – while() e Funções de String
 Faça um Exercício em PHP Ler 3 números, 
 os possíveis lados de um triângulo, 
-e imprimir a classificação de acordo com tamanho dos lados. -->
+e imprimir a classificação de acordo com tamanho dos lados. 
+-->
+
+<?php
+
+echo "Insira 3 números para descobrir qual triângulo é. \n Insira o primeiro número: ";
+$n1 = readline();
+
+echo "Insira o segundo número: ";
+$n2 = readline();
+
+echo "Insira o terceiro número: ";
+$n3 = readline();
+
+if ($n1 == $n2 && $n2 == $n3) {
+  echo "Triângulo Equilátero.";
+} elseif ($n1 == $n2 || $n2 == $n3 || $n1 == $n3) {
+  echo "Triângulo Isósceles.";
+} else {
+  echo "Triângulo Escaleno.";
+}
+
+?>
