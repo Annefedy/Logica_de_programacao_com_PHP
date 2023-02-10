6f7da975bb19043d32cd2a1f9fe07894267ba37d
annefedy <annecarine1997@gmail.com>
2023-02-10 08:04:47 -0300

    Question 18

--- Question18.php
+++ Question18.php
@@ -4,3 +4,28 @@ a multiplicação entre B e D
 e verifique se o resultado da soma é maior, 
 menor ou igual ao da multiplicação.
 Imprima: "A+C é maior que B+D", "A+C é menor que B+D", "A+C é igual a B+D". -->
+
+<?php
+echo "Digite um valor A: ";
+$A = readline();
+
+echo "Digite um valor B: ";
+$B = readline();
+
+echo "Digite um valor C: ";
+$C = readline();
+
+echo "Digite um valor D: ";
+$D = readline();
+
+$soma = $A + $C;
+$multiplicacao = $B * $D;
+
+if ($soma > $multiplicacao) {
+  echo "A+C é maior que B+D";
+} elseif ($soma < $multiplicacao) {
+  echo "A+C é menor que B+D";
+} else {
+  echo "A+C é igual a B+D";
+}
+?>
