487a46939330a6ea67a695208e918c10eebc3811
annefedy <annecarine1997@gmail.com>
2023-02-10 09:26:12 -0300

    Question 24

--- Question24.php
+++ Question24.php
@@ -1,4 +1,21 @@
 <!-- 24 - UTILIZAR LAÇOS DE REPETIÇÃO – for() – while() e Funções de String
 Faça um algoritmo PHP que receba duas strings, 
 e descubra se elas são iguais ou diferentes, 
-imprimindo “IGUAL” ou “DIFERENTE” respectivamente. -->
+imprimindo “IGUAL” ou “DIFERENTE” respectivamente. 
+-->
+
+<?php
+
+echo "Digite qualquer palavra: ";
+$string1 = readline();
+
+echo "Digite a mesma palavra ou uma palavra diferente: ";
+$string2 = readline();
+
+if (strcmp($string1, $string2) == 0) {
+  echo "IGUAL";
+} else {
+  echo "DIFERENTE";
+}
+
+?>
